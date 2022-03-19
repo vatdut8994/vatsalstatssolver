baaeb3f3a9facf0023ec94e08007e0607c1dadae
vatdut8994 <82714982+vatdut8994@users.noreply.github.com>
2022-03-19 15:47:26 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from matplotlib import pyplot as plt
 from flask import Flask, render_template, request, send_file
@@ -23,19 +23,31 @@ def predict():
     if request.method == 'POST':
         global pyplot
         plt.grid()
-        y = str(request.form['traindata'])
-        list1 = y.split(',')
-        for i in range(len(list1)):
-            list1[i] = float(list1[i])
-        y = list1
-        x = str(request.form['testdata'])
-        list1 = x.split(',')
-        for i in range(len(list1)):
-            list1[i] = float(list1[i])
-        x = list1
-        graphing = int(request.form['graphing'])
-        l1=(x[0], y[0])
-        l2 = (x[-1], y[-1])
+        try:
+            y = str(request.form['traindata'])
+            list1 = y.split(',')
+            for i in range(len(list1)):
+                list1[i] = float(list1[i])
+            y = list1
+            x = str(request.form['testdata'])
+            list1 = x.split(',')
+            for i in range(len(list1)):
+                list1[i] = float(list1[i])
+            x = list1
+            graphing = int(request.form['graphing'])
+            test = str(request.form['answer'])
+            list1 = test.split(',')
+            for i in range(len(list1)):
+                list1[i] = float(list1[i])
+            test = list1
+        except:
+            x = [1,2,3,4,5]
+            y = [1,2,3,4,5]
+            l1=(x[0], y[0])
+            graphing = 0
+            l2 = (x[-1], y[-1])
+            test = [10]
+        
         try:
             centraltendency = str(request.form['card'])
             list1 = centraltendency.split(',')
@@ -44,19 +56,19 @@ def predict():
             centraltendency = list1
             statans=f"Mean: {stats.mean(centraltendency)},\nMedian: {stats.median(centraltendency)},\nMode: {stats.mode(centraltendency)},\nStandard Deviation: {stats.stdev(centraltendency)}\n"
         except:
-            statans= ''
+            statans = ''
+
         if graphing == 1:
             pyplot = plt.plot(x, y)
         elif graphing == 0:
             pyplot = plt.scatter(x, y)
         line_of_best_fit = plt.plot(np.unique(x), np.poly1d(np.polyfit(x, y, 1))(np.unique(x)))
+        plt.show()
         plt.savefig('./static/images/new_plot.png')
         data = pd.DataFrame({"x":x, "y":y })
         list1 = line_of_best_fit[0].get_data()
-        print(x)
         x = list1[0]
         y = list1[1]
-        print(y)
         line_table = pd.DataFrame({'x':x, 'y':y})
 
         print("Data: \n", data)
@@ -68,14 +80,8 @@ def predict():
 
         xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.2)
 
-        model = RandomForestRegressor(max_depth=5)
+        model = LinearRegression()
         model.fit(xtrain, ytrain)
-
-        test = str(request.form['answer'])
-        list1 = test.split(',')
-        for i in range(len(list1)):
-            list1[i] = float(list1[i])
-        test = list1
         print("Answer: ", model.predict([test]))
         img = BytesIO()
         plt.savefig(img, format='png')
